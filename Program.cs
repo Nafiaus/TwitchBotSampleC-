10f434a2bc6d2095272859089e008f9cb442d4d5
Cody <46328153+Nafiaus@users.noreply.github.com>
2024-02-27 22:48:12 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Pico.Twitch;
+
+namespace TwitchBot
+{
+
+    class Program
+    {
+
+        static string oauth = "oauth:yevs6k8g77wgrl9j62788olkc56ozm";
+        static string channelName = "nafiaus";
+        
+        static TwitchChatHandle chat = new TwitchChatHandle(oauth, channelName);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var chatEntry = chat.Read();
+                CheckCommand(chatEntry);
+
+            }
+        }
+
+        static void CheckCommand(TwitchChatEntry chatEntry)
+        {
+            string msg = chatEntry.Message;
+            switch(msg)
+            {
+                case "!hello":
+                    chat.Write("Hello World");
+                    break;
+            }
+        }
+    }
+}
